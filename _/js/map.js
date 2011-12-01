8afac5209b1d48853289db469bfbadc1d68a768b
Mark Learst <learst@me.com>
2011-12-01 15:41:55 -0500

    Adding a few base js files

---
+++ _/js/map.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+	function buildMap() {
+		var lat = 42.315718;
+		var lng = -83.216801;
+		var latlng = new google.maps.LatLng(lat, lng); 
+		// Creating an object literal containing the properties we want to pass to the map  
+		var options = {  
+			zoom: 15,
+			center: latlng,
+			scrollwheel: false,
+			panControl: false,
+			rotateControl: false,
+			scaleControl: false,
+			zoomControl: false,
+			mapTypeControlOptions: {
+				style: google.maps.MapTypeControlStyle.DEFAULT
+			},
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		};  
+		// Calling the constructor, thereby initializing the map  
+		var map = new google.maps.Map(document.getElementById('map'), options);  
+		
+		var marker = new google.maps.Marker({
+			position: new google.maps.LatLng(lat, lng), 
+			map: map,
+			title: 'Team Detroit',
+			clickable: true,
+			icon: 'img/tdi-map-icon.png'
+		});
+		
+		$(window).resize(function(event) {
+			map.setCenter(latlng, 15);
+		});
+		
+	}
+
+	buildMap();
+
+
+});
