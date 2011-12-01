1f20bb12e5f421ebc66e39706ea7426fe61c976f
Mark Learst <learst@me.com>
2011-12-01 17:39:01 -0500

    Base plugin I need to finish up

---
+++ _/js/jquery.fluidCarousel.0.1.js
@@ -0,0 +1,20 @@
+// going to be creating the carousel plugin with infinite carousel, pagenate count, video capabilities and ect...
+(function($) {
+
+	$.fn.fluidCarousel = function(options) {
+
+		var settings = $.extend({
+			
+			speed: 500,
+			buffer: 2,
+			timer: false
+			
+		}, options);
+
+		return this.each(function() {
+
+		});
+
+	};
+	
+})(jQuery);
