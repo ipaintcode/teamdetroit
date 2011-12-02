bbdea3bffcca33b9be1dac4b6eb16c2093f89567
Mark Learst <learst@me.com>
2011-12-02 00:27:48 -0500

    map.js update

--- _/js/map.js
+++ _/js/map.js
@@ -18,6 +18,7 @@ $(document).ready(function() {
 				style: google.maps.MapTypeControlStyle.DEFAULT
 			},
 			mapTypeId: google.maps.MapTypeId.ROADMAP
+			
 		};
 		// Calling the constructor, thereby initializing the map
 		var map = new google.maps.Map(document.getElementById('map'), options);
@@ -33,60 +34,38 @@ $(document).ready(function() {
 		$(window).resize(function(event) {
 			map.setCenter(latlng, 15);
 		});
-
 	}
-
 	buildMap();
-
 });
 
-var GMAPs = (function() {
-	var lat = 42.315718,	// default lat
-		lng = -83.216801,	// default lng
-		latlng = new google.maps.LatLng(lat, lng),
-		options = {
-			zoom: 15,
-			center: latlng,
-			scrollwheel: false,
-			panControl: false,
-			rotateControl: false,
-			scaleControl: false,
-			zoomControl: false,
-			mapTypeControlOptions: {
-				style: google.maps.MapTypeControlStyle.DEFAULT
-			},
-			mapTypeId: google.maps.MapTypeId.ROADMAP
-		},
-		map = new google.maps.Map(document.getElementById('map'), options),
-		marker = new google.maps.Marker({
-			position: new google.maps.LatLng(lat, lng),
-			map: map,
-			title: 'Team Detroit',
-			clickable: true,
-			icon: 'img/tdi-map-icon.png'
-		});
-		
-		
-		
-		
-		
-			
-			
-			
-			
-							
-							
-		$(window).resize(function(event) {
-			map.setCenter(latlng, 15);
-		});
-
-})();
-
-
-
-
-
-
-
-
-
+// var GMAPs = (function() {
+// 	var lat = 42.315718,	// default lat
+// 		lng = -83.216801,	// default lng
+// 		latlng = new google.maps.LatLng(lat, lng),
+// 		options = {
+// 			zoom: 15,
+// 			center: latlng,
+// 			scrollwheel: false,
+// 			panControl: false,
+// 			rotateControl: false,
+// 			scaleControl: false,
+// 			zoomControl: false,
+// 			mapTypeControlOptions: {
+// 				style: google.maps.MapTypeControlStyle.DEFAULT
+// 			},
+// 			mapTypeId: google.maps.MapTypeId.ROADMAP
+// 		},
+// 		map = new google.maps.Map(document.getElementById('map'), options),
+// 		marker = new google.maps.Marker({
+// 			position: new google.maps.LatLng(lat, lng),
+// 			map: map,
+// 			title: 'Team Detroit',
+// 			clickable: true,
+// 			icon: 'img/tdi-map-icon.png'
+// 		});	
+// 
+// 		$(window).resize(function(event) {
+// 			map.setCenter(latlng, 15);
+// 		});
+// 
+// })();
