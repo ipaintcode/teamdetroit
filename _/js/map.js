99fe111371adde0814e02fa93a52411d09e8c0e7
Mark Learst <learst@me.com>
2011-12-01 22:21:33 -0500

    Base JavaScript lib maintenance

--- _/js/map.js
+++ _/js/map.js
@@ -3,9 +3,9 @@ $(document).ready(function() {
 	function buildMap() {
 		var lat = 42.315718;
 		var lng = -83.216801;
-		var latlng = new google.maps.LatLng(lat, lng); 
-		// Creating an object literal containing the properties we want to pass to the map  
-		var options = {  
+		var latlng = new google.maps.LatLng(lat, lng);
+		// Creating an object literal containing the properties we want to pass to the map
+		var options = {
 			zoom: 15,
 			center: latlng,
 			scrollwheel: false,
@@ -17,25 +17,24 @@ $(document).ready(function() {
 				style: google.maps.MapTypeControlStyle.DEFAULT
 			},
 			mapTypeId: google.maps.MapTypeId.ROADMAP
-		};  
-		// Calling the constructor, thereby initializing the map  
-		var map = new google.maps.Map(document.getElementById('map'), options);  
-		
+		};
+		// Calling the constructor, thereby initializing the map
+		var map = new google.maps.Map(document.getElementById('map'), options);
+
 		var marker = new google.maps.Marker({
-			position: new google.maps.LatLng(lat, lng), 
+			position: new google.maps.LatLng(lat, lng),
 			map: map,
 			title: 'Team Detroit',
 			clickable: true,
 			icon: 'img/tdi-map-icon.png'
 		});
-		
+
 		$(window).resize(function(event) {
 			map.setCenter(latlng, 15);
 		});
-		
+
 	}
 
 	buildMap();
-
 
 });
