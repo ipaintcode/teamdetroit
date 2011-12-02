605b1dd8f185ce7af636ca8716cc7d9865a6a452
Mark Learst <learst@me.com>
2011-12-01 23:00:22 -0500

    maps.js convert

--- _/js/map.js
+++ _/js/map.js
@@ -1,5 +1,6 @@
 // Need to create a Module Pattern called; GMAPS and only init it on pages that have map
 $(document).ready(function() {
+	
 	function buildMap() {
 		var lat = 42.315718;
 		var lng = -83.216801;
@@ -38,3 +39,54 @@ $(document).ready(function() {
 	buildMap();
 
 });
+
+var GMAPs = (function() {
+	var lat = 42.315718,	// default lat
+		lng = -83.216801,	// default lng
+		latlng = new google.maps.LatLng(lat, lng),
+		options = {
+			zoom: 15,
+			center: latlng,
+			scrollwheel: false,
+			panControl: false,
+			rotateControl: false,
+			scaleControl: false,
+			zoomControl: false,
+			mapTypeControlOptions: {
+				style: google.maps.MapTypeControlStyle.DEFAULT
+			},
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		},
+		map = new google.maps.Map(document.getElementById('map'), options),
+		marker = new google.maps.Marker({
+			position: new google.maps.LatLng(lat, lng),
+			map: map,
+			title: 'Team Detroit',
+			clickable: true,
+			icon: 'img/tdi-map-icon.png'
+		});
+		
+		
+		
+		
+		
+			
+			
+			
+			
+							
+							
+		$(window).resize(function(event) {
+			map.setCenter(latlng, 15);
+		});
+
+})();
+
+
+
+
+
+
+
+
+
