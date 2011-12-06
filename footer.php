d119cf12e6879cee9f0491f3c021bc9674a51844
Mark Learst <learst@me.com>
2011-12-06 12:49:30 -0500

    adding footer.php

---
+++ footer.php
@@ -0,0 +1,57 @@
+<footer class="footer fade-second">
+	<div class="center min-max">
+		<aside class="teamdetroit-facts-box single">
+			<!-- <div class="logo-white-headline">Team Detorit</div> -->
+			<!-- <ul>
+							<li>rotate goodies about TDi</li>
+							<li>rotate goodies about TDi</li>
+							<li>rotate goodies about TDi</li>
+							<li>rotate goodies about TDi</li>
+							<li>rotate goodies about TDi</li>
+						</ul> -->
+			<img src="img/facts.png" alt="Facts" style="display: block;">
+			<!-- <span class="est-time">2:35<sup>am</sup></span>
+			<span class="est-weather-temp">72&deg;</span>
+			<span class="est-weather-icon"></span> -->
+		</aside>
+		<ul class="footer-nav double">
+			<li class="twentyfive">
+				<h5>Work</h5>
+				<ul class="large">
+					<li><a href="#">Unexpected Press Conference</a></li>
+					<li><a href="#">Go Do</a></li>
+					<li><a href="#">Focus Rally</a></li>
+					<li><a href="#">CCS</a></li>
+					<li><a href="#">WDET 101.2</a></li>
+				</ul>
+			</li>
+			<li class="twentyfive">
+				<h5>About Us</h5>
+				<ul class="large">
+					<li><a href="#">Who We Are</a></li>
+					<li><a href="#">What We Do</a></li>
+					<li><a href="#">How We Do It</a></li>
+					<li><a href="#">Privacy Policy</a></li>
+				</ul>
+			</li>
+			<li class="twentyfive">
+				<h5>Culture</h5>
+				<ul class="large">
+					<li><a href="#">Warriors in Pink</a></li>
+					<li><a href="#">People Mover Project</a></li>
+					<li><a href="#">Greening of Detroit</a></li>
+					<li><a href="#">Movember</a></li>
+					<li><a href="#">Team Detroit Idol</a></li>
+				</ul>
+			</li>
+			<li class="twentyfive">
+				<h5>Voicebox</h5>
+				<ul class="large">
+					<li><a href="#">Planting Trees in Detroit</a></li>
+					<li><a href="#">Mobile is takign over</a></li>
+					<li><a href="#">And the winner is</a></li>
+				</ul>
+			</li>
+		</ul>
+	</div>
+</footer>
