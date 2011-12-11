705a60260d072c939ffabc1b0530c71968f19537
Mark Learst <learst@me.com>
2011-12-11 01:45:40 -0500

    finalized template for landing page and some css updates for that template

--- leadership_template.php
+++ leadership_template.php
@@ -66,41 +66,63 @@
 <div class="wrapper"><!-- not needed? up to you: http://camendesign.com/code/developpeurs_sans_frontieres -->
 
 	<?php include 'header.php'; ?>
-
+	
+	<div id="billboard-spacer"></div>
+	
 	<div class="billboard-wrapper billboard-ext-leaders">
 		<ul>
-			<li><img src="_/img/leadership/brad.jpg" alt=""></li>
-			<li><img src="_/img/leadership/brad.jpg" alt=""></li>
-			<li><img src="_/img/leadership/brad.jpg" alt=""></li>
+			<li><img src="_/img/leadership/monik_sanghvi.jpg" alt="Monik Sanghvi"></li>
+			<li><img src="_/img/leadership/satish_korde.jpg" alt="Satish Korde"></li>
+			<li><img src="_/img/leadership/scott_lange.jpg" alt="Scott Lange"></li>
 		</ul>
-	</div>
-		
 	</div>
 
 	<div class="segment-headline-wrapper clear">
-		<h2 class="center min-max">Toby Barlow<span class="segment-leader-title">(ECD)</span></h2>
+		<h2 class="center min-max"><span class="segment-header-spacer">Toby Barlow</span><span class="segment-leader-title">(ECD)</span></h2>
 	</div>
 
 	<div class="content-wrapper">
 
-		<div class="content-inner center min-max">
+		<div class="content center min-max">
 
-			<section class="content double">
+			<section class="content-info-wrapper double">
 
-				<p>One of the first things you hear when entering the advertising industry to make a living is that you better have a thick skin and be willing to fight for what you believe in. Brad Audet was obviously taking notes on the first day. His competitive nature, stemming from a background in collegiate athletics, has helped him take on any new challenge, turn it into a winner and come back looking for more. That’s part of why he is the Group Managing Director of Team Detroit.</p>
+				<div class="content-info">
 
-				<p>In that role, Brad leads new-business development and is the brand steward for Team Detroit. He oversees a number of accounts including Shell, Scotts Miracle Gro, Bosch, Compuware, Johnson Controls and Warrior Sports. Prior to his current responsibilities, he led the successful launch of the Ford “Drive One” campaign and seven nameplate launches.</p>
+					<p>One of the first things you hear when entering the advertising industry to make a living is that you better have a thick skin and be willing to fight for what you believe in. Brad Audet was obviously taking notes on the first day. His competitive nature, stemming from a background in collegiate athletics, has helped him take on any new challenge, turn it into a winner and come back looking for more. That’s part of why he is the Group Managing Director of Team Detroit.</p>
 
-				<p>He didn’t just walk into this role. Brad used to run the Mullen Detroit office servicing several General Motors businesses. He also helped open Frankel’s Detroit office to provide brand activation support for General Motors. But it all started at Ross Roy, where he worked on Kmart, Builders Square, OfficeMax and AT&amp;T.</p>
+					<p>In that role, Brad leads new-business development and is the brand steward for Team Detroit. He oversees a number of accounts including Shell, Scotts Miracle Gro, Bosch, Compuware, Johnson Controls and Warrior Sports. Prior to his current responsibilities, he led the successful launch of the Ford “Drive One” campaign and seven nameplate launches.</p>
 
+					<p>He didn’t just walk into this role. Brad used to run the Mullen Detroit office servicing several General Motors businesses. He also helped open Frankel’s Detroit office to provide brand activation support for General Motors. But it all started at Ross Roy, where he worked on Kmart, Builders Square, OfficeMax and AT&amp;T.</p>
+					
+					<p>He didn’t just walk into this role. Brad used to run the Mullen Detroit office servicing several General Motors businesses. He also helped open Frankel’s Detroit office to provide brand activation support for General Motors. But it all started at Ross Roy, where he worked on Kmart, Builders Square, OfficeMax and AT&amp;T.</p>
+				
+					<ul class="content-info-social">
+						<li>Network &rarr;</li>
+						<li class="linkedin"><a href="#linkedin">LinkedIn</a></li>
+					</ul>
+
+				</div>
+				
 			</section>
 
-			<aside class="single">
+			<aside class="content-quote-wrapper single">
 
-				<p>"When you don’t script people, you get the honesty and credibility you just can’t get from a traditional ad."</p>
-				<p>&ndash;Matt Van Dyke<span>Ford U.S. Marketing Director</span></p>
+				<div class="content-quote">
+
+					 <blockquote>
+					 	<p>When you don’t script people, you get the honesty and credibility you just can’t get from a traditional ad.</p>
+					 	<p class="content-quote-author">&ndash; Matt Van Dyke <span>(Ford U.S. Marketing Director)</span></p>
+					 </blockquote>
+				
+				</div>
 
 			</aside>
+			
+			<ul class="secondary-nav">
+				<li class="secondary-nav-prev"><a href="#previous">Previous</a></li>
+				<li class="secondary-nav-next"><a href="#next">Next</a></li>
+			</ul>
 		
 		</div>
 		
