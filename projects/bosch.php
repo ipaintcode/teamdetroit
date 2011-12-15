498ddbc290a9fe4857bf0b0e61b1dfbf1d9db941
Mark Learst <learst@me.com>
2011-12-15 14:15:51 -0500

    .

--- projects/bosch.php
+++ projects/bosch.php
@@ -83,6 +83,13 @@
 
 	<div class="segment-headline-wrapper clear">
 		<h2 class="center min-max"><span class="segment-header-spacer">Bosch Storm Chasers TV:30</span></h2>
+		<div class="carousel-navigation center min-max">
+			<ul class="carousel-controller">
+				<li class="prev-button"><a href="#prev">&larr; Previous</a></li>
+				<li class="page-nate"><span class="page-of">1</span> of <span>12</span></li>
+				<li class="next-button"><a href="#next">Next &rarr;</a></li>
+			</ul>
+		</div>
 	</div>
 
 	<div class="content-wrapper">
