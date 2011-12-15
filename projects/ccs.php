8686cc2c47b7d31881d10f780016e30ce4601d1a
Mark Learst <learst@me.com>
2011-12-15 13:22:19 -0500

    .

--- projects/ccs.php
+++ projects/ccs.php
@@ -94,11 +94,13 @@
 		</ul>
 	</div>
 	
-	<ul class="carousel-controller">
-		<li class="prev-button"><a href="#prec">&larr; Previous</a></li>
-		<li class="page-nate">1 of <span>12</span></li>
-		<li class="next-button"><a href="#next">Next &rarr;</a></li>
-	</ul>
+	<div class="carousel-navigation center min-max">
+		<ul class="carousel-controller">
+			<li class="prev-button"><a href="#prec">&larr; Previous</a></li>
+			<li class="page-nate">1 of <span>12</span></li>
+			<li class="next-button"><a href="#next">Next &rarr;</a></li>
+		</ul>
+	</div>
 
 	<div class="segment-headline-wrapper clear">
 		<h2 class="center min-max"><span class="segment-header-spacer">CCS</span></h2>
