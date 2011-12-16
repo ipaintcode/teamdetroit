3c4b31f976dcf255f690014abddf99a64f80cc8d
Mark Learst <learst@me.com>
2011-12-15 20:41:40 -0500

    ,

--- _/js/functions.js
+++ _/js/functions.js
@@ -137,8 +137,8 @@ $(document).ready(function (){
 				animating = true;
 				$('.fluidCarousel li:first').before($('.fluidCarousel li:last'));
 				var ulLeft = $('.fluidCarousel').css('left');
-				console.log(parseInt(ulLeft, 10)+$('.fluidCarousel li:last').width());
-				$('.fluidCarousel').css('left', parseInt(ulLeft, 10)+$('.fluidCarousel li:last').width());
+				console.log(parseInt(ulLeft, 10)-$('.fluidCarousel li:last').width());
+				$('.fluidCarousel').css('left', parseInt(ulLeft, 10)-$('.fluidCarousel li:last').width());
 				if($('.activeCarousel').prev().length === 0) {
 					$('.fluidCarousel li').eq(len-1).addClass('activeCarousel');
 				}
