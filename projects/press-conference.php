0e5a1e1e6544ce17eee12581055fd9f65ab78db1
Mark Learst <learst@me.com>
2011-12-17 15:33:21 -0500

    .

--- projects/press-conference.php
+++ projects/press-conference.php
@@ -61,12 +61,13 @@
 	<style>
 		.gridbox {
 			position: absolute;
-			left: -50px;
+			left: -50px; top: 0;
 			background: url('../_/img/grid.png') no-repeat center center;
 			border-left: 1px dashed #C4C4C4;
 			border-right: 1px dashed #C4C4C4;
 			width; 50px;
 			height: 100%;
+			text-indent: -9999px;
 		}
 	</style>
 </head>
