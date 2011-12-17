4a0f1731b59a0483bc533604618e396a21254ae6
Mark Learst <learst@me.com>
2011-12-17 16:00:21 -0500

    .

--- _/js/functions.js
+++ _/js/functions.js
@@ -5,6 +5,12 @@
 /* trigger when page is ready */
 $(document).ready(function (){
 	
+	$('.gridbox').hover(function() {
+		$(this).stop().animate({opacity: .5}, 300);
+	}, function() {
+		$(this).stop().animate({opacity: 1}, 300);
+	});
+	
 	$('.wrapper').stop().delay(500).animate({
 		opacity: 1
 	}, 500);
