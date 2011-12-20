8a90398979929e1d630bceb3593d3fc593bb01bf
Mark Learst <learst@me.com>
2011-12-20 14:27:54 -0500

    .

--- _/js/jquery.fluidCarousel.0.1.js
+++ _/js/jquery.fluidCarousel.0.1.js
@@ -1,20 +1,60 @@
-// going to be creating the carousel plugin with infinite carousel, pagenate count, video capabilities and ect...
-(function($) {
-
-	$.fn.fluidCarousel = function(options) {
-
-		var settings = $.extend({
-			
-			speed: 500,
-			buffer: 2,
-			timer: false
-			
-		}, options);
-
-		return this.each(function() {
-
+$(document).ready(function() {
+	$(window).load(function() {
+		$('.fluidCarousel li img').each(function(index) {
+			width +=$(this).width();
 		});
+		$('.fluidCarousel').width(width);			
+		var w = ($(window).width()-$('.fluidCarousel').width())/2;
 
-	};
-	
-})(jQuery);
+		
+		function resetCarousel() {
+			var windownWidth = ($(window).width() > 960) ? $(window).width() : 960;
+			$('.fluidCarousel').stop().animate({
+				left: ((windownWidth-$('.activeCarousel').width())/2)-($('.activeCarousel').offset().left-$('.fluidCarousel').offset().left)
+			}, 500, function() {
+				animating = false;
+			});
+		}
+		
+		var deletedCount = 0;
+		
+		$(window).resize(function(event) {
+			resetCarousel();
+		});
+		
+		var len = $('.fluidCarousel li').length-1;
+		for (var i = 0; i < len+1; i++) {
+		
+			$('.fluidCarousel li').eq(len).clone().prependTo('.fluidCarousel').removeClass('activeCarousel');
+		}
+		$('.fluidCarousel li img').each(function(index) {
+			width +=$(this).width();
+		});
+		$('.fluidCarousel').width(width);
+		$('.fluidCarousel').css({
+			left: (($(window).width()-$('.activeCarousel').width())/2)-($('.activeCarousel').offset().left-$('.fluidCarousel').offset().left)
+		});
+		
+		var animating = false;
+		$('.billboard-ext-carousel').click(function(event) {
+		// if (deletedCount < 3) {
+		// 		$('.fluidCarousel li:last').remove();
+		// 	}
+		deletedCount+=1;
+		if(!animating) {
+			animating = true;
+			$('.fluidCarousel li:last').after($('.fluidCarousel li:first'));
+			var ulLeft = $('.fluidCarousel').css('left');
+			$('.fluidCarousel').css('left', $('.fluidCarousel li:last').width()+parseInt(ulLeft, 10));	
+			if($('.activeCarousel').next().length === 0) {
+					$('.fluidCarousel li').eq(1).addClass('activeCarousel');
+				}
+				$('.activeCarousel').removeClass('activeCarousel').next().addClass('activeCarousel');
+				resetCarousel();
+			}
+		});
+		
+		resetCarousel();
+		$('.billboard-ext-carousel ul').stop().animate({opacity: 1}, 500);
+	});
+});
--- careers.php
+++ careers.php
@@ -364,9 +364,10 @@
 <!-- here comes the javascript -->
 
 <!-- Grab Google CDN's jQuery. fall back to local if necessary -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.min.js"></script>
-<script>window.jQuery || document.write("<script src='_/js/jquery-1.6.4.min.js'>\x3C/script>")</script>
+<script>window.jQuery || document.write("<script src='_/js/jquery-1.7.1.min.js'>\x3C/script>")</script>
+
 
 <!-- this is where we put our custom functions -->
 <script src="_/js/jquery.shadow-animation.min.js"></script>
