1edb46ade764b6eab58bbf71e9fafe6973cfdd75
Mark Learst <Mark@Marks-MacBook-Pro.local>
2011-12-22 11:27:18 -0500

    .

--- index.php
+++ index.php
@@ -145,6 +145,7 @@
 						<img src="_/img/projects/carhartt/thumb.jpg" alt="">
 					</a>
 				</li>
+				
 				<li class="single">
 					<a href="projects/focus-rally.php">
 						<h3>Focus Rally</h3>
