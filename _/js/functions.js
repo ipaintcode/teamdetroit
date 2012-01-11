df93f024cf5a2cbd639379cf149382d2b83b5e26
Mark Learst <learst@me.com>
2012-01-11 12:44:27 -0500

    .

--- _/js/functions.js
+++ _/js/functions.js
@@ -148,22 +148,16 @@ $(document).ready(function (){
 	        (navigator.platform.indexOf("iPod") != -1)
 	    );
 	}
-
-
-	// ;
-	// video.src = "undefined";
-
+	
 	function wdetPlayer() {
 		var audio = '<audio controls autoplay="autoplay" src="http://141.217.119.35:8000/;&amp;topspeed=on"></audio>';
 		var flash = "<embed src='http://www.shoutcast.com/media/popupPlayer_V19.swf?stationid=http://yp.shoutcast.com/sbin/tunein-station.pls?id=307298&play_status=1' quality='high' bgcolor='#ffffff' width='398' height='104' name='popupPlayer_V19' align='middle' allowScriptAccess='always' allowFullScreen='true' type='application/x-shockwave-flash' pluginspage='http://www.adobe.com/go/getflashplayer' ></embed>"
 		if ($.cookie("wdet") === "0") {
-			// video.src = "undefined";
 			$('.player-holder').html("");
 			$('.add-stream').css('opacity', 0).text('Start Listening').stop().animate({
 				opacity: 1
 			}, 500);
 		} else {
-			// video.src = url;
 			if ($.browser.msie === true || $.browser.mozilla === true) {
 				$('.player-holder').html(flash);
 			} else {
