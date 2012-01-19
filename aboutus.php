1a8202515a1e23fbc5f38c9dc025ea3ca36ad7f9
Mark Learst <learst@me.com>
2012-01-19 11:50:02 -0500

    .

--- aboutus.php
+++ aboutus.php
@@ -312,9 +312,9 @@
 				<p class="policy-title">Team Detroit Privacy Policy</p>
 				<p class="policy-body">As a communications agency, our most important message is that we value your privacy. View our policy here.</p>
 				<p class="policy-arrow">&darr;</p>
-				<p class="policy-links"><a href="_/doc/TDI Website Internet Privacy Policy.pdf">Download Privacy Policy</a></p>
-				<p class="policy-links"><a href="_/doc/Safe Harbor PII Policy.pdf">Download Safe Harbor Policy</a></p>
-				<p class="policy-links"><a href="_/doc/TDI Website Terms and Conditions of Use.pdf">Download Terms and Conditions</a></p>
+				<p class="policy-links"><a href="_/doc/TDI Website Internet Privacy Policy.pdf" target="_blank">Download Privacy Policy</a></p>
+				<p class="policy-links"><a href="_/doc/Safe Harbor PII Policy.pdf" target="_blank">Download Safe Harbor Policy</a></p>
+				<p class="policy-links"><a href="_/doc/TDI Website Terms and Conditions of Use.pdf" target="_blank">Download Terms and Conditions</a></p>
 			</div>
 
 		</div>
