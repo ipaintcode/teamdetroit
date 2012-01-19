8708787b121d2cff7dfd0667507a45b46b6899a0
Mark Learst <learst@me.com>
2012-01-19 05:58:22 -0500

    .

--- _/js/functions.js
+++ _/js/functions.js
@@ -38,7 +38,7 @@ jQuery.cookie = function(key, value, options) {
 $(document).ready(function (){
 	
 	function updateTime() {
-		$.get('time.php', function(data) {
+		$.get('../time.php', function(data) {
 			$('.est-time').html(data)
 		});
 		setTimeout(updateTime, 5000);
